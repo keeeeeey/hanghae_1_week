0f90bb0c4f01fcd3ade5ba6e4887b9e50f97ce7d
jsw4215 <jsw4215van@gmail.com>
2022-01-10 20:55:10 +0900

    search

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 from pymongo import MongoClient
 
 client = MongoClient('mongodb://test:test@localhost', 27017)
+# client = MongoClient('localhost', 27017)
 db = client.dbhomework
 
 
@@ -28,6 +29,19 @@ def write():
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+
+## 글쓰기화면 보여주기
+@app.route('/api/search', methods=['POST'])
+def search():
+    words_receive = request.form['words_give']
+
+    find_list = list(db.article.find({'title':{'$regex':words_receive, '$options' : 'i'}},{'_id': False}))
+
+    for i in range(len(find_list)):
+        print("find : " + str(find_list[i]))
+
+    return jsonify({'searched_list': find_list})
 
 
 if __name__ == '__main__':
