d6c5626f966768b94bfa400802dfaf89482fbfe4
sunny-yo <dmsfh3@gmail.com>
2022-01-10 17:28:25 +0900

    Add login UI & login funtionality

--- app.py
+++ app.py
@@ -1,20 +1,48 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, session, redirect, url_for
+from pymongo import MongoClient
+import jwt
+import datetime
+import hashlib
 
 app = Flask(__name__)
-
-from pymongo import MongoClient
-
 client = MongoClient('localhost', 27017)
-db = client.dbhomework
+db = client.dbsparta_plus_week4
+
+SECRET_KEY = 'SPARTA'
 
 ## HTML 화면 보여주기
 @app.route('/')
 def homework():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/sign_in')
 def login():
     return render_template('login.html')
+
+## 로그인 API
+@app.route('/sign_in', methods=['POST'])
+def sign_in():
+    # 로그인
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+
+    print(username_receive)
+    print(password_receive)
+
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
+
+    if result is not None:
+        payload = {
+        'id': username_receive,
+        'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
+
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=8000, debug=True)
