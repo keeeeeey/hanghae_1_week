3c725663765da4ee775785760cf1005e2c68885f
jsw4215 <jsw4215van@gmail.com>
2022-01-10 18:47:02 +0900

    port 8000 -> 5000, mongoDB, ellipsis

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 from pymongo import MongoClient
 
-client = MongoClient('localhost', 27017)
+client = MongoClient('mongodb://test:test@localhost', 27017)
 db = client.dbhomework
 
 
@@ -31,4 +31,4 @@ def login():
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=8000, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
