bd9db383da62077818f7db97f06755b0dc2d1c4a
keeeeeey <sseioul@naver.com>
2022-01-11 11:05:31 +0900

    read.html2

--- app.py
+++ app.py
@@ -1,28 +1,157 @@
-import hashlib
-
-from flask import Flask, render_template, jsonify, request
-
-app = Flask(__name__)
+from flask import Flask, render_template, jsonify, request, session, redirect, url_for
 
 from pymongo import MongoClient
 
+import jwt
+
+from datetime import datetime, timedelta
+
+import hashlib
+
+app = Flask(__name__)
+
+# client = MongoClient('mongodb://test:test@localhost', 27017)
+
+app = Flask(__name__)
 client = MongoClient('localhost', 27017)
 db = client.dbhanghae
+
+SECRET_KEY = 'SPARTA'
+
 
 ## HTML 화면 보여주기
 @app.route('/')
 def homework():
+    print("home start")
     return render_template('index.html')
+
+    question_list = list(db.article.find({}))
+    id_list = []
+
+    #id값을 가져올 수 있도록 articles의 ObjectId로 되어있는 _id를 str형식으로 변경한다.
+    for item in question_list:
+        id_list.append(str(item['_id']))
+
+    for i in range(len(question_list)):
+        del question_list[i]['_id']
+        question_list[i]['_id'] = id_list[i]
+
+    # jwt token 받아오기
+    token_receive = request.cookies.get('mytoken')
+
+    if token_receive is None:
+        print("비로그인 to index")
+        return render_template('index.html', list=question_list)
+    else:
+        print("로그인 to index")
+        try:
+            payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+            user_id = db.users.find_one({"id": payload['id']})['id']
+
+            return render_template('index.html', list=question_list, userId=user_id)
+        except jwt.ExpiredSignatureError:
+            return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+        except jwt.exceptions.DecodeError:
+            return redirect(url_for("login"))
+
+
+## 글쓰기화면 보여주기
+@app.route('/write')
+def move_write():
+    token_receive = request.cookies.get('mytoken')
+    payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+    #지정된 token id filed
+    user_info = db.article.find_one({"user_id": payload['id']})
+    return render_template('write.html', user_id = user_info["user_id"])
+
+@app.route('/api/posting', methods=['POST'])
+def write_post():
+    id_receive = request.form['id_give']
+    title_receive = request.form['title_give']
+    content_receive = request.form['content_give']
+
+    doc = {
+        'user_id': id_receive,
+        'title': title_receive,
+        'content': content_receive,
+    }
+
+    db.article.insert_one(doc)
+
+    return jsonify({'result': 'success', 'msg': '질문 등록 완료!!'})
+
+## 글쓰기화면 보여주기
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+## 글쓰기화면 보여주기
+@app.route('/readPage')
+def readPage():
+    return render_template('read.html')
+
+## 글쓰기화면 보여주기
+@app.route('/api/search', methods=['POST'])
+def search():
+    words_receive = request.form['words_give']
+
+    find_list = list(db.article.find({'title':{'$regex':words_receive, '$options' : 'i'}}))
+
+    id_list = []
+
+    # id값을 가져올 수 있도록 articles의 ObjectId로 되어있는 _id를 str형식으로 변경한다.
+    for item in find_list:
+        id_list.append(str(item['_id']))
+
+    for i in range(len(find_list)):
+        del find_list[i]['_id']
+        find_list[i]['_id'] = id_list[i]
+
+    return jsonify({'searched_list': find_list})
+
+
+## 글쓰기화면 보여주기
+@app.route('/api/read')
+def read():
+
+    article_id = request.args.get('article_id')
+    user_id = request.args.get('user_id')
+
+    target_article = db.article.find_one({'_id': article_id})
+    reply_on_article = list(db.reply.find({'article_id', article_id}))
+
+    return render_template('read.html', target_article=target_article, reply_on_article=reply_on_article)
+
 
 ## register 화면 보여주기
 @app.route('/registerPage')
 def registerPage():
     return render_template('register.html')
 
-## register 화면 보여주기
-@app.route('/readPage')
-def readPage():
-    return render_template('read.html')
+## 로그인 API
+@app.route('/api/login', methods=['POST'])
+def sign_in():
+    # 로그인
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+
+    print(username_receive)
+    print(password_receive)
+
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
+
+    if result is not None:
+        payload = {
+        'id': username_receive,
+        'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
+
+        return jsonify({'result': 'success', 'mytoken': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
 ## id 중복검사
 @app.route('/api/idCheck', methods=['POST'])
